be0a3a8ff9719708cc62a12db091551ba91c2f5f
David Passmore <davcpas1234@users.noreply.github.com>
2021-05-31 13:18:40 +0100

    Update pathing

--- src/LaravelPdfViewerServiceProvider.php
+++ src/LaravelPdfViewerServiceProvider.php
@@ -24,7 +24,7 @@ class LaravelPdfViewerServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'../laraview' => public_path('public'),
+            __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'laraview' => public_path('laraview'),
         ], 'public');
     }
 
